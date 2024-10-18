ec72c02a67b0c7de54c041a699a0b8cc8392e200
Andy-Vasseur <andyvasseurpro@outlook.com>
2024-10-18 13:13:04 +0200

    :sparkles: added react-router, and a Homepage.tsx

--- front_chat-app/src/App.tsx
+++ front_chat-app/src/App.tsx
@@ -1,9 +1,19 @@
+// Imports
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import Homepage from "./pages/Homepage";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
+    </div>
   );
 }
 
+// Exports
 export default App;
---
+++ front_chat-app/src/pages/Homepage/index.tsx
@@ -0,0 +1,10 @@
+function Homepage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Homepage</h1>
+    </div>
+  );
+}
+
+// Exports
+export default Homepage;
